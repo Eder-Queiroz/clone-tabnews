314a39aa65fedb48803a66ef0d1830a2355804f4
Eder Queiroz <eder.queiroz@graodireto.com.br>
2025-12-10 22:33:20 -0300

    mudando a mesangem exibida

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
+import Image from "next/image";
+
 function Home() {
-
-  return <h1>Teste</h1>;
-
+  return (
+    <>
+      <h1>Nara, eu amo você. Se você me ama, dá uma risadinha</h1>
+      <Image src="/foto-nara-lucky.jpeg" width={200} height={320} />
+    </>
+  );
 }
 
 export default Home;
